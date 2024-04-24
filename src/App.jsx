ce9e0490df2dc845419cdcf76a9294e70eda1c55
Alexei <>
2024-04-24 14:29:13 +0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Layout } from 'antd';
+import {Layout} from 'antd';
 
-const { Header, Footer, Content, Sider } = Layout;
+const {Header, Footer, Sider, Content} = Layout;
 
 const headerStyle = {
     textAlign: 'center',
     color: '#fff',
+    height: 64,
+    paddingInline: 48,
     lineHeight: '64px',
     backgroundColor: '#4096ff',
 };
 
 const contentStyle = {
     textAlign: 'center',
-    minHeight: 'calc(100vh - 64px)',
+    minHeight: 120,
+    lineHeight: '120px',
     color: '#fff',
     backgroundColor: '#0958d9',
+};
+
+const siderStyle = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#1677ff',
 };
 
 const footerStyle = {
@@ -24,14 +34,19 @@ const footerStyle = {
 };
 
 const layoutStyle = {
+    borderRadius: 8,
+    overflow: 'hidden',
     width: '100vw',
-    height: '100vh',
+    height: '100vh'
 };
 
 const App = () => (
     <Layout style={layoutStyle}>
         <Header style={headerStyle}>Header</Header>
-        <Content style={contentStyle}>Content</Content>
+        <Layout>
+            <Sider width="25%" style={siderStyle}>Sider</Sider>
+            <Content style={contentStyle}>Content</Content>
+        </Layout>
         <Footer style={footerStyle}>Footer</Footer>
     </Layout>
 );
