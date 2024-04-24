f0201ebad8774bc9d58e6f2394b50ab8691e80d2
Alexei <>
2024-04-24 14:25:26 +0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
+import React from 'react';
+import { Layout } from 'antd';
 
-import './App.css'
-import { Button } from 'antd';
+const { Header, Footer, Content, Sider } = Layout;
+
+const headerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    lineHeight: '64px',
+    backgroundColor: '#4096ff',
+};
+
+const contentStyle = {
+    textAlign: 'center',
+    minHeight: 'calc(100vh - 64px)',
+    color: '#fff',
+    backgroundColor: '#0958d9',
+};
+
+const footerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#4096ff',
+};
+
+const layoutStyle = {
+    width: '100vw',
+    height: '100vh',
+};
 
 const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
+    <Layout style={layoutStyle}>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>Content</Content>
+        <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
 );
 
 export default App;
